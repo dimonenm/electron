4149bb76aa220e55c144edc48da18fd6f460e31e
exp <arzuakov@yahoo.com>
2022-04-18 00:01:38 +0300

    h18.04.2022

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, screen, BrowserWindow, Menu, MenuItem } from 'electron';
 
 // const lock = app.releaseSingleInstanceLock();
 // console.log('lock: ', lock);
@@ -13,15 +13,116 @@ import { app, BrowserWindow } from 'electron';
 //     }
 //   })
 // }
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      { role: 'quit' }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...[
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ]
+    ]
+  },
+  new MenuItem({
+    label: app.name,
+    submenu: [
+      new MenuItem({
+        label: 'Option 1',
+        click() {
+          console.log('Option 1 clicked')
+        }
+      }),
+      new MenuItem({
+        type: 'separator'
+      }),
+      new MenuItem({
+        label: 'Option 2',
+        click() {
+          console.log('Option 2 clicked')
+        }
+      })
+    ]
+  }),
+  {
+    label: 'test',
+    submenu: [
+      {
+        label: 'Option 1',
+        click() {
+          console.log('Option 1 clicked')
+        }
+      },
+      {
+        label: 'Option 2',
+        click() {
+          console.log('Option 2 clicked')
+        }
+      }
+    ]
+  },
+]
 
-app.whenReady().then(() => {
+const ctxTemplate = [
+  {label: 'Op 1'},
+  {label: 'Op 2'},
+  {type: 'separator'},
+  {label: 'Op 3'},
+]
+const ctxMenu = Menu.buildFromTemplate(ctxTemplate);
+
+const createMenu = () => {
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+const createWindow = () => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 400,
+    minHeight: 400,
+    maxWidth: width,
+    maxHeight: height,
+    show: false,
+    // titleBarStyle: "hidden",
+    // titleBarOverlay: {
+    //   color: 'cadetblue',
+    //   symbolColor: '#000000'
+    // },
     webPreferences: {
-      nodeIntegration: false
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
     }
   })
   win.loadFile('renderer/index.html');
-  win.webContents.openDevTools()
+
+  win.on('ready-to-show', () => {
+    win.show();
+  })
+  win.webContents.on('context-menu', (event, params) => {
+    ctxMenu.popup(win, params.x, params.y)
+  })
+
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createMenu();
+  createWindow();  
 });
